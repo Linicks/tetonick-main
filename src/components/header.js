931e3cc31fedf29153a2cc8f1df812cb313b4251
Linicks <linicks@gmail.com>
2019-12-25 10:48:13 -0700

    Fix style sheet issues.

--- src/components/header.js
+++ src/components/header.js
@@ -2,44 +2,41 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-import logo from "../images/tetonick-logo5.png";
+import logo from "../images/tetonick-logo5.png"
 
-import { Block } from 'baseui/block';
+import { Block } from "baseui/block"
 
 import {
   HeaderNavigation,
   ALIGN,
   StyledNavigationList,
-  StyledNavigationItem
-} from "baseui/header-navigation";
-import { StyledLink } from "baseui/link";
+  StyledNavigationItem,
+} from "baseui/header-navigation"
+import { StyledLink } from "baseui/link"
 
-export const HEADER_BREAKPOINT = '@media screen and (min-width: 607px)';
+export const HEADER_BREAKPOINT = "@media screen and (min-width: 607px)"
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
       background: `white`,
       marginBottom: `0.45rem`,
-      marginTop: '0.45rem'
+      marginTop: "0.45rem",
     }}
   >
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `0 0.55rem 0 0`,
+        padding: `0 0 0 0`,
       }}
     >
       <HeaderNavigation>
-
         <StyledNavigationList $align={ALIGN.left}>
           <StyledNavigationItem>
             <Link to="/">
-              <img src={logo} alt="Tetonick Logo" width="100%"
-                height="auto"/>
+              <img src={logo} alt="Tetonick Logo" width="90%" height="auto" />
             </Link>
-
           </StyledNavigationItem>
         </StyledNavigationList>
 
@@ -48,27 +45,23 @@ const Header = ({ siteTitle }) => (
           overrides={{
             Block: {
               style: {
-                display: 'none',
+                display: "none",
                 [HEADER_BREAKPOINT]: {
-                  display: 'block',
+                  display: "block",
                 },
               },
             },
           }}
-
           width="100%"
-          padding="1.75rem 0.55rem 0 0"
-
+          padding="1.55rem 0.0rem 0.55rem 0"
         >
-
           <StyledNavigationList $align={ALIGN.right}>
-
             <StyledNavigationItem>
               <StyledLink href="/"> Home </StyledLink>
             </StyledNavigationItem>
 
             <StyledNavigationItem>
-              <StyledLink href="/learn/"> Learn  </StyledLink>
+              <StyledLink href="/learn/"> Learn </StyledLink>
             </StyledNavigationItem>
 
             <StyledNavigationItem>
@@ -76,13 +69,9 @@ const Header = ({ siteTitle }) => (
             </StyledNavigationItem>
 
             <StyledNavigationItem></StyledNavigationItem>
-
           </StyledNavigationList>
-
         </Block>
-
       </HeaderNavigation>
-
     </div>
 
     <Block
@@ -90,27 +79,24 @@ const Header = ({ siteTitle }) => (
       overrides={{
         Block: {
           style: {
-            display: 'block',
+            display: "block",
             [HEADER_BREAKPOINT]: {
-              display: 'none',
+              display: "none",
             },
           },
         },
       }}
-
       width="100%"
-      padding="1.55rem 0.0rem 0.55rem 0.0rem "
+      padding="1.0rem 0.0rem 0.55rem 0.0rem "
       backgroundColor="#eeeeee"
     >
-
       <StyledNavigationList $align={ALIGN.center}>
-
         <StyledNavigationItem>
           <StyledLink href="/"> Home </StyledLink>
         </StyledNavigationItem>
 
         <StyledNavigationItem>
-          <StyledLink href="/learn/"> Learn  </StyledLink>
+          <StyledLink href="/learn/"> Learn </StyledLink>
         </StyledNavigationItem>
 
         <StyledNavigationItem>
@@ -118,11 +104,9 @@ const Header = ({ siteTitle }) => (
         </StyledNavigationItem>
 
         <StyledNavigationItem></StyledNavigationItem>
-
       </StyledNavigationList>
     </Block>
-
-  </header >
+  </header>
 )
 
 Header.propTypes = {
